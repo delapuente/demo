2635addfc67ba39393380a17860e68c25c837273
Salvador de la Puente <salva@unoyunodiez.com>
2015-04-07 09:33:32 +0200

    Changing and url by a relative one

--- js/game/main.js
+++ js/game/main.js
@@ -3,7 +3,7 @@
   'use strict';
 
   var likwit = new Phaser.Game(640, 960, Phaser.AUTO);
-  likwit.state.add('level', new LikwitLevel('/maps/0005.map'));
+  likwit.state.add('level', new LikwitLevel('./maps/0005.map'));
 
   likwit.state.start('level');
 }());
