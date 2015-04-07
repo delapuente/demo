29c654335a8e1fa287720989b65d458ab1b51ef0
Salvador de la Puente <salva@unoyunodiez.com>
2015-04-07 09:49:32 +0200

    Select level by changing hash

--- js/game/main.js
+++ js/game/main.js
@@ -3,7 +3,28 @@
   'use strict';
 
   var likwit = new Phaser.Game(640, 960, Phaser.AUTO);
-  likwit.state.add('level', new LikwitLevel('./maps/0005.map'));
 
-  likwit.state.start('level');
+  [1, 2, 3, 4, 5].forEach(function (levelNo) {
+    var levelName = getLevelName(levelNo);
+    likwit.state.add(
+      levelName,
+      new LikwitLevel('./maps/' + levelName  + '.map')
+    );
+  });
+
+  var currentLevel = getLevelName(window.location.hash.substr(1) || 1);
+  likwit.state.start(currentLevel);
+
+  window.onhashchange = function () {
+    var newLevel = getLevelName(window.location.hash.substr(1) || 1);
+    likwit.state.start(newLevel);
+    currentLevel = newLevel;
+  };
+
+  function getLevelName(n) {
+    n = '' + n;
+    while (n.length < 4) { n = '0' + n; }
+    return n;
+  }
+
 }());
